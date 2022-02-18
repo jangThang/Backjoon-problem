3ca462c3523d645178b5a18d5c5f92216f0d4e52
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-02-18 11:36:11 +0900

    Create 10768 특별한 날.py

---
+++ 10768 특별한 날.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+month = int(input())
+day = int(input())
+
+if month == 1:
+    print("Before")
+elif month == 2 and day < 18:
+    print("Before")
+elif month == 2 and day == 18:
+    print("Special")
+else:
+    print("After")
