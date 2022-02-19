380aa4bda57f0e7076c2881548ae46878e35f37d
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-02-20 00:53:16 +0900

    Create 1927 최소 힙.py

---
+++ 1927 최소 힙.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+    # x가 0이 아니면 push
+    if x != 0:
+        heapq.heappush(heap, x)
+
+    # x가 0이고 비어있지 않으면 pop
+    elif heap:
+        print(heapq.heappop(heap))
+
+    # x가 0이고 비어있으면 0 출력
+    else:
+        print(0)
