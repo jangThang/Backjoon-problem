bfcf778dca2f5d1f960adc8ef65f3beb6dff28d0
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-02-28 04:42:59 +0900

    Create 15080 Every Second Counts.py

---
+++ 15080 Every Second Counts.py
@@ -0,0 +1,27 @@
+start = list(map(int, input().split(':')))
+end = list(map(int, input().split(':')))
+
+res = 0 #두 시각의 차
+
+#초 차이
+if start[2] <= end[2]:
+    res += end[2] - start[2]
+else:
+    end[1] -= 1
+    res += end[2] - start[2] + 60
+
+#분 차이
+if start[1] <= end[1]:
+    res += (end[1] - start[1])*60
+else:
+    end[0] -= 1
+    res += (end[1] - start[1] + 60)*60
+
+#시 차이
+if start[0] <= end[0]:
+    res += (end[0] - start[0])*3600
+else:
+    end[0] += 24
+    res += (end[0] - start[0])*3600
+
+print(res)
