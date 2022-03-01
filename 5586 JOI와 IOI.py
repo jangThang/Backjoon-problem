6113b60d644868310fa28794e66b41ce32acd0fe
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-03-01 09:34:58 +0900

    Create 5586 JOI와 IOI.py

---
+++ 5586 JOI와 IOI.py
@@ -0,0 +1,11 @@
+string = input()
+joi = 0
+ioi = 0
+
+for i in range(len(string)-2):
+    if string[i:i+3] == 'JOI':
+        joi += 1
+    if string[i:i+3] == 'IOI':
+        ioi += 1
+print(joi)
+print(ioi)
