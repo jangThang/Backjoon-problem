fd9af24ce7d4745986e62bc0b708af05e550a379
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-03-03 00:45:22 +0900

    Create 2720 세탁소 사장 동혁.py

---
+++ 2720 세탁소 사장 동혁.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    change = int(input()) #거스름돈
+
+    quarter = change//25
+    change %= 25
+
+    dime = change//10
+    change %= 10
+
+    nickel = change//5
+    change %= 5
+
+    penny = change
+    print(quarter, dime, nickel, penny)
