fa614c79f6c2f9bb996da0230ea1bd0a981721c3
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-04-27 13:53:58 +0900

    Create 13623 Zero or One.py

---
+++ 13623 Zero or One.py
@@ -0,0 +1,12 @@
+# 입력
+a, b, c = map(int, input().split())
+
+# 출력
+if (a == 1 and b == c == 0) or (a == 0 and b == c == 1):
+    print("A")
+elif (b == 1 and a == c == 0) or (b == 0 and a == c == 1):
+    print("B")
+elif (c == 1 and a == b == 0) or (c == 0 and a == b == 1):
+    print("C")
+else:
+    print("*")
