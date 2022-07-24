a0932feda049fe9bfec9d5670b9bd67e258983c9
Jang WooYoung <48477448+jangThang@users.noreply.github.com>
2022-07-24 18:15:38 +0900

    Create 17618 신기한 수.py

---
+++ 17618 신기한 수.py
@@ -0,0 +1,10 @@
+n = int(input())
+res = 0
+for number in range(1, n+1):
+    tmp = str(number)
+    digit_sum = 0
+    for i in tmp:
+        digit_sum += int(i)
+    if number % digit_sum == 0:
+        res += 1
+print(res)
